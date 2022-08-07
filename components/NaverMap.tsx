47504a34ca19f6b773829de57db7bc6dcbafc246
inojeon <inojeon@kisti.re.kr>
2022-08-08 00:45:51 +0900

    add useInitNaverMap

--- components/NaverMap.tsx
+++ components/NaverMap.tsx
@@ -1,35 +1,7 @@
-import useImsp from "lib/client/useIMSP";
-import { useEffect } from "react";
+import useInitNaverMap from "lib/client/useInitNaverMap";
 
 export default function NaverMap() {
-  const { ok: imspOk, data: imspData } = useImsp();
-  // console.log(imspData);
-
-  useEffect(() => {
-    if (imspOk && imspData?.longitude && imspData?.latitude) {
-      const initMap = () => {
-        const map = new naver.maps.Map("map", {
-          center: new naver.maps.LatLng(
-            imspData?.longitude,
-            imspData?.latitude
-          ),
-          zoom: 15,
-          // zoom: imspData?.zoom,
-          minZoom: 7, //지도의 최소 줌 레벨
-          zoomControl: true, //줌 컨트롤의 표시 여부
-          zoomControlOptions: {
-            //줌 컨트롤의 옵션
-            position: naver.maps.Position.TOP_RIGHT,
-          },
-          scaleControl: true,
-          logoControl: true,
-          mapDataControl: true,
-          mapTypeControl: true,
-        });
-      };
-      initMap();
-    }
-  }, [imspOk, imspData]);
+  useInitNaverMap();
 
   //지도 사이즈 관련 스타일
   const mapStyle = {
